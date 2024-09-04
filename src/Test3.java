ead8405a19dc755909ca6d463d8230461784e6ef
ganeshtidake09 <ganeshtidake09@gmail.com>
2024-09-04 16:47:55 +0530

    Third Commit

---
+++ src/Test3.java
@@ -0,0 +1,17 @@
+
+public class Test3 {
+
+	public static void main(String[] args) 
+	{
+		
+		String hungry = "No";
+		if(hungry =="NO" || hungry == "Y" || hungry=="N")
+		{
+			System.out.println("Eat Something>>>>>>");
+		}
+		else
+			System.out.println("Do work");
+
+	}
+
+}
